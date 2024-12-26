3a8571084ea194fd98554c5f57338f74fd08927f
Maksym <maksymgushcha20@gmail.com>
2024-12-26 17:29:08 +0200

    Implemented ProductService with mapping and CRUD operations

--- src/main/java/com/example/spacecatsmarket/service/ProductService.java
+++ src/main/java/com/example/spacecatsmarket/service/ProductService.java
@@ -1,2 +1,31 @@
-package com.example.spacecatsmarket.service;public class ProductService {
+package com.example.spacecatsmarket.service;
+
+import com.example.spacecatsmarket.domain.Product;
+import com.example.spacecatsmarket.dto.ProductDTO;
+import com.example.spacecatsmarket.web.mapper.ProductMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductMapper productMapper;
+
+    private final List<Product> products = new ArrayList<>();
+
+    public ProductService(ProductMapper productMapper) {
+        this.productMapper = productMapper;
+    }
+
+    public List<ProductDTO> getAllProducts() {
+        return products.stream()
+                .map(productMapper::toDTO)
+                .toList();
+    }
+
+    public void addProduct(ProductDTO productDTO) {
+        products.add(productMapper.toEntity(productDTO));
+    }
 }
