dddd3624c4e6431c26340b69fbf0503d5bd8612c
Maksym <maksymgushcha20@gmail.com>
2024-12-26 18:07:32 +0200

    Added aspect for feature toggle logic

--- src/main/java/com/example/spacecatsmarket/featuretoggle/FeatureToggleAspect.java
+++ src/main/java/com/example/spacecatsmarket/featuretoggle/FeatureToggleAspect.java
@@ -1,2 +1,30 @@
-package com.example.spacecatsmarket.featuretoggle;public class FeatureToggleAspect {
+package com.example.spacecatsmarket.featuretoggle;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FeatureToggleAspect {
+
+    private final FeatureToggleService featureToggleService;
+
+    public FeatureToggleAspect(FeatureToggleService featureToggleService) {
+        this.featureToggleService = featureToggleService;
+    }
+
+    @Before("@annotation(CosmoCatsFeature)")
+    public void checkCosmoCatsFeature() {
+        if (!featureToggleService.isCosmoCatsEnabled()) {
+            throw new FeatureNotAvailableException("CosmoCats feature is disabled!");
+        }
+    }
+
+    @Before("@annotation(KittyProductsFeature)")
+    public void checkKittyProductsFeature() {
+        if (!featureToggleService.isKittyProductsEnabled()) {
+            throw new FeatureNotAvailableException("KittyProducts feature is disabled!");
+        }
+    }
 }
