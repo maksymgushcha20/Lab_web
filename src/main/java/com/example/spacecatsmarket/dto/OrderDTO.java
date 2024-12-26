8ff6367df938063dffff3bed92f67b0b640899c9
Maksym <maksymgushcha20@gmail.com>
2024-12-26 17:29:03 +0200

    Updated OrderDTO with required fields

--- src/main/java/com/example/spacecatsmarket/dto/OrderDTO.java
+++ src/main/java/com/example/spacecatsmarket/dto/OrderDTO.java
@@ -1,2 +1,13 @@
-package com.example.spacecatsmarket.dto;public class OrderDTO {
+package com.example.spacecatsmarket.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class OrderDTO {
+    private Long id;
+    private List<Long> productIds; // Список ID продуктів
+    private Double totalPrice;
+    private LocalDateTime orderDate;
 }
