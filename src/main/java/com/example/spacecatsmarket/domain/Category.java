bbb07f0315dcdf5db61dbd593beca9319ee16a22
Maksym <maksymgushcha20@gmail.com>
2024-12-26 17:28:47 +0200

    Updated Category domain model

---
+++ src/main/java/com/example/spacecatsmarket/domain/Category.java
@@ -0,0 +1,10 @@
+package com.example.spacecatsmarket.domain;
+
+import lombok.Data;
+
+@Data
+public class Category {
+    private Long id;
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/example/spacecatsmarket/web/mapper/CategoryMapper.java
@@ -0,0 +1,2 @@
+package com.example.spacecatsmarket.web.mapper;public interface CategoryMapper {
+}
