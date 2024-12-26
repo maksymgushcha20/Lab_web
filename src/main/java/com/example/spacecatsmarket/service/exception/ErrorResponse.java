b46ea9e1d19b9da07e690ebbd55afe959c313ff7
Maksym <maksymgushcha20@gmail.com>
2024-12-26 17:20:48 +0200

    Created ErrorResponse class for standardized error responses

--- src/main/java/com/example/spacecatsmarket/service/exception/ErrorResponse.java
+++ src/main/java/com/example/spacecatsmarket/service/exception/ErrorResponse.java
@@ -1,2 +1,16 @@
-package com.example.spacecatsmarket.service.exception;public class ErrorResponse {
+package com.example.spacecatsmarket.service.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private LocalDateTime timestamp;
 }
