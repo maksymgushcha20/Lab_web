ed93808d6229dc875ca61f72478476aa8756dd3b
Maksym <maksymgushcha20@gmail.com>
2024-12-26 18:07:17 +0200

    Implemented feature toggle for Cosmo Cats

---
+++ src/main/java/com/example/spacecatsmarket/featuretoggle/FeatureNotAvailableException.java
@@ -0,0 +1,2 @@
+package com.example.spacecatsmarket.featuretoggle;public class FeatureNotAvailableException {
+}
---
+++ src/main/java/com/example/spacecatsmarket/service/CatService.java
@@ -0,0 +1,2 @@
+package com.example.spacecatsmarket.service;public class CatService {
+}
