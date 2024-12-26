c7b1a9a2ef680a8cb23a92618ac9e995cb986e6f
Maksym <maksymgushcha20@gmail.com>
2024-12-26 17:29:12 +0200

    Implemented CategoryMapper for mapping between domain and DTO

--- src/main/java/com/example/spacecatsmarket/web/mapper/CategoryMapper.java
+++ src/main/java/com/example/spacecatsmarket/web/mapper/CategoryMapper.java
@@ -1,2 +1,11 @@
-package com.example.spacecatsmarket.web.mapper;public interface CategoryMapper {
+package com.example.spacecatsmarket.web.mapper;
+
+import com.example.spacecatsmarket.domain.Category;
+import com.example.spacecatsmarket.dto.CategoryDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    CategoryDTO toDTO(Category category);
+    Category toEntity(CategoryDTO categoryDTO);
 }
