2f1e733965c0d9f24bca3d22fc345a4ccab5a185
Maksym <maksymgushcha20@gmail.com>
2024-12-26 17:29:16 +0200

    Implemented ProductMapper for mapping between domain and DTO

--- src/main/java/com/example/spacecatsmarket/web/mapper/ProductMapper.java
+++ src/main/java/com/example/spacecatsmarket/web/mapper/ProductMapper.java
@@ -1,2 +1,15 @@
-package com.example.spacecatsmarket.web.mapper;public interface ProductMapper {
+package com.example.spacecatsmarket.web.mapper;
+
+import com.example.spacecatsmarket.domain.Product;
+import com.example.spacecatsmarket.dto.ProductDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    @Mapping(source = "category.id", target = "categoryId")
+    ProductDTO toDTO(Product product);
+
+    @Mapping(source = "categoryId", target = "category.id")
+    Product toEntity(ProductDTO productDTO);
 }
