04bdebca575576757519e60f4269b21df12f7137
Maksym <maksymgushcha20@gmail.com>
2024-12-26 18:07:39 +0200

    Added base service for cats

--- src/main/java/com/example/spacecatsmarket/service/CatService.java
+++ src/main/java/com/example/spacecatsmarket/service/CatService.java
@@ -1,2 +1,19 @@
-package com.example.spacecatsmarket.service;public class CatService {
+package com.example.spacecatsmarket.service;
+
+import com.example.spacecatsmarket.featuretoggle.CosmoCatsFeature;
+import com.example.spacecatsmarket.featuretoggle.KittyProductsFeature;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CatService {
+
+    @CosmoCatsFeature
+    public void handleCosmoCats() {
+        System.out.println("Handling CosmoCats...");
+    }
+
+    @KittyProductsFeature
+    public void handleKittyProducts() {
+        System.out.println("Handling KittyProducts...");
+    }
 }
