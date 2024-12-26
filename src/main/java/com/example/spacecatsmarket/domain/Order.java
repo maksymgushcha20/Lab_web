625dfb959081e6ce86affe8d94a575cdf05608e7
Maksym <maksymgushcha20@gmail.com>
2024-12-26 17:28:51 +0200

    Updated Order domain model

--- src/main/java/com/example/spacecatsmarket/domain/Order.java
+++ src/main/java/com/example/spacecatsmarket/domain/Order.java
@@ -1,2 +1,13 @@
-package com.example.spacecatsmarket.domain;public class Order {
+package com.example.spacecatsmarket.domain;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class Order {
+    private Long id;
+    private List<Product> products;
+    private Double totalPrice;
+    private LocalDateTime orderDate;
 }
