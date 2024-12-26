bfad97c55ede69859f26b822edce3c335769dbd8
Maksym <maksymgushcha20@gmail.com>
2024-12-26 18:07:42 +0200

    Added service for Cosmo Cats with feature toggle integration

--- src/main/java/com/example/spacecatsmarket/service/CosmoCatService.java
+++ src/main/java/com/example/spacecatsmarket/service/CosmoCatService.java
@@ -1,2 +1,13 @@
-package com.example.spacecatsmarket.service;public class CosmoCatService {
+package com.example.spacecatsmarket.service;
+
+import com.example.spacecatsmarket.featuretoggle.CosmoCatsFeature;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CosmoCatService {
+
+    @CosmoCatsFeature
+    public String getCosmoCats() {
+        return "List of CosmoCats from the galaxy!";
+    }
 }
